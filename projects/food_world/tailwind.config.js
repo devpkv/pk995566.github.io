a56d8210e6b8ec841fa9c9f027badd02ef22cb3e
Pankaj Kumar <54941511+pk995566@users.noreply.github.com>
2021-05-20 12:17:44 +0530

    Add files via upload

---
+++ projects/food_world/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#EF5350',
+        secondory: {
+          100: '#9E9E9E',
+          200: '#424242'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
